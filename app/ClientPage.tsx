479af12f2257467b36935e4f34131a51b5c7024f
Alexis Pineda <ixspnd.ap@gmail.com>
2026-08-13 03:55:57 +0800

    quick animation

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -1,19 +1,26 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { useGLTF, OrbitControls, Environment } from '@react-three/drei';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls, Environment } from "@react-three/drei";
 
 // 1. Create a component to load and render the model
 function MyModel() {
   // useGLTF inherently takes care of caching the loaded resources
-  const { scene } = useGLTF('/models/RAM-2500HD-CC-696j-Body.glb');
+  const { scene } = useGLTF("/models/RAM-2500HD-CC-696j-Body.glb");
+  const [height, setHeight] = useState(1);
   
+  useEffect(() => {
+    setInterval(() => {
+        setHeight((prevHeight) => (prevHeight <= 0.5 ? prevHeight : prevHeight - 0.01));
+    }, 100);
+  }, [])
+
   // The primitive component displays the raw Three.js object in the scene
-  return <primitive object={scene} scale={1} position={[0.25, 0.5, 0.65]} />;
+  return <primitive object={scene} scale={1} position={[0.25, height, 0.65]} />;
 }
 function MyModel2() {
   // useGLTF inherently takes care of caching the loaded resources
-  const { scene } = useGLTF('/models/RAM-2500HD-CC.glb');
-  
+  const { scene } = useGLTF("/models/RAM-2500HD-CC.glb");
+
   // The primitive component displays the raw Three.js object in the scene
   return <primitive object={scene} scale={1} />;
 }
@@ -27,7 +34,7 @@ export default function ModelViewer() {
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
         <Environment preset="city" />
-        
+
         {/* Suspense is required while the external model file downloads */}
         <Suspense fallback={null}>
           <MyModel />
