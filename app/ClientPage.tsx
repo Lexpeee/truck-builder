f91619a346a5722ae89ed4e448934547d67315db
Alexis Pineda <ixspnd.ap@gmail.com>
2026-08-17 22:58:07 +0800

    fix: hdri issues

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, OrbitControls, Environment } from "@react-three/drei";
@@ -6,13 +7,7 @@ import { useGLTF, OrbitControls, Environment } from "@react-three/drei";
 function MyModel() {
   // useGLTF inherently takes care of caching the loaded resources
   const { scene } = useGLTF("/models/RAM-2500HD-CC-696j-Body.glb");
-  const [height, setHeight] = useState(1);
-  
-  useEffect(() => {
-    setInterval(() => {
-        setHeight((prevHeight) => (prevHeight <= 0.5 ? prevHeight : prevHeight - 0.05));
-    }, 100);
-  }, [])
+  const [height, setHeight] = useState(0.5);
 
   // The primitive component displays the raw Three.js object in the scene
   return <primitive object={scene} scale={1} position={[0.25, height, 0.65]} />;
@@ -33,7 +28,7 @@ export default function ModelViewer() {
         {/* Lights are required to see the material properly */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
-        <Environment preset="city" />
+        <Environment files="/hdri/grasslands_sunset_4k.hdr"/>
 
         {/* Suspense is required while the external model file downloads */}
         <Suspense fallback={null}>
